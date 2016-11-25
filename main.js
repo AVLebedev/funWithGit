4f5c37c1ec1e49bc795af00c2159fb3924e6446c
AVLebedev <val5-94@mail.ru>
2016-11-25 14:42:46 +0400

    Я создал, я создал!

---
+++ main.js
@@ -0,0 +1,15 @@
+﻿//Выполняет сложение чисел и выводит результат
+function getSum(){
+var a = document.getElementById('a').value;
+var b = document.getElementById('b').value;
+var res = Number(a) + Number(b);
+	document.getElementById("resOut").textContent=res;
+}
+
+//Выполняет вычитание чисел и выводит результат
+function getMul(){
+}
+
+//Выполняет деление чисел и выводит результат
+function getDiv(){
+}
