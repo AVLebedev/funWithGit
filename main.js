c1414ffcfec24be250c6e362ea83a1deba748e42
freeman1396 <yakushev1396@gmail.com>
2016-11-26 20:20:21 +0300

    Calculations function

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 ﻿//Выполняет сложение чисел и выводит результат
 function getSum(){
-var a = document.getElementById('a').value;
-var b = document.getElementById('b').value;
-var res = Number(a) + Number(b);
+	var a = document.getElementById('a').value;
+	var b = document.getElementById('b').value;
+	var res = Number(a) + Number(b);
+	document.getElementById("resOut").textContent=res;
+}
+//Выполняет вычитание чисел и выводит результат
+function getSubtraction(){
+	var a = document.getElementById('a').value;
+	var b = document.getElementById('b').value;
+	var res = Number(a) - Number(b);
+	document.getElementById("resOut").textContent=res;
+}
+//Выполняет умножение чисел и выводит результат
+function getMultiplication(){
+	var a = document.getElementById('a').value;
+	var b = document.getElementById('b').value;
+	var res = Number(a) * Number(b);
 	document.getElementById("resOut").textContent=res;
 }
 
-//Выполняет вычитание чисел и выводит результат
-function getMul(){
-}
-
 //Выполняет деление чисел и выводит результат
-function getDiv(){
+function getDivision(){
+	var a = document.getElementById('a').value;
+	var b = document.getElementById('b').value;
+	var res = Number(a) / Number(b);
+	document.getElementById("resOut").textContent=res;
 }
